dbb7b51e05971cd3e5f52596d72d3d4d2398c6a5
Esbaevnurdos <nurdosesbaev82@gmail.com>
2025-05-30 21:49:15 +0500

    replace

--- db/db.js
+++ db/db.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, // Render requires SSL
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
     console.error("Database connection error:", err);
