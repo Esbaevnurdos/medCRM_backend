e37ab6ece59d7d3803e515a45c7f56e543e6e26d
Esbaevnurdos <nurdosesbaev82@gmail.com>
2025-05-31 19:11:31 +0500

    kerekemes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const captchaController = require("../controllers/captchaController");
 
 router.post("/register", authController.registerUser);
 router.post("/request-otp", authController.requestOTP);
 router.post("/set-password", authController.verifyAndSetPassword);
 router.post("/login", authController.loginUser);
 router.post("/logout", authController.logoutUser);
-router.get("/captcha", generateCaptcha);
+router.get("/captcha", captchaController.generateCaptcha);
 
 module.exports = router;
