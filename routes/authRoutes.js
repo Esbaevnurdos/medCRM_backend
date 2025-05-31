cd8fcc665c4d476b81ae5475ab4948a04ee7c73f
Esbaevnurdos <nurdosesbaev82@gmail.com>
2025-05-31 15:51:38 +0500

    please

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("admin/register", authController.registerUser);
-router.post("admin/request-otp", authController.requestOTP);
-router.post("admin/set-password", authController.verifyAndSetPassword);
-router.post("admin/login", authController.loginUser);
-router.post("admin/logout", authController.logoutUser);
+router.post("/register", authController.registerUser);
+router.post("/request-otp", authController.requestOTP);
+router.post("/set-password", authController.verifyAndSetPassword);
+router.post("/login", authController.loginUser);
+router.post("/logout", authController.logoutUser);
 
 module.exports = router;
